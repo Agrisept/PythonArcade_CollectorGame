03915d421cdd0bea53748c982746edf3ea402351
1-мд-8 Невмержицкая Ксения <99484143+Agrisept@users.noreply.github.com>
2023-05-20 22:15:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+
+import pygame
+from pygame.locals import *
+
+# импортируйте pygame.locals для упрощения доступа к ключевым координатам
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Collector"
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+
+
+# Define our square object and call super to
+# give it all the properties and methods of pygame.sprite.Sprite
+# Define the class for our square objects
+class Square(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Square, self).__init__()
+
+        # Define the dimension of the surface
+        # Here we are making squares of side 25px
+        self.surf = pygame.Surface((25, 25))
+
+        # Define the color of the surface using RGB color coding.
+        self.surf.fill((0, 200, 255))
+        self.rect = self.surf.get_rect()
+
+
+# initialize pygame
+pygame.init()
+
+# Define the dimensions of screen object
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), RESIZABLE)
+
+# instantiate all square objects
+square1 = Square()
+square2 = Square()
+square3 = Square()
+square4 = Square()
+
+# Variable to keep our game loop running
+gameOn = True
+
+# Our game loop
+while gameOn:
+    # for loop through the event queue
+    for event in pygame.event.get():
+
+        # Check for KEYDOWN event
+        if event.type == KEYDOWN:
+
+            # If the Backspace key has been pressed set
+            # running to false to exit the main loop
+            if event.key == K_BACKSPACE:
+                gameOn = False
+
+        # Check for QUIT event
+        elif event.type == QUIT:
+            gameOn = False
+
+    # Define where the squares will appear on the screen
+    # Use blit to draw them on the screen surface
+    screen.blit(square1.surf, (40, 40))
+    screen.blit(square2.surf, (40, 530))
+    screen.blit(square3.surf, (730, 40))
+    screen.blit(square4.surf, (700, 530))
+
+    # Update the display using flip
+    pygame.display.update()
